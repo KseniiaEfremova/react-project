304c551f2df6e838f76123a1bfc77f6d63e1a34f
KseniiaEfremova <ef.xenya@yandex.ru>
2023-04-15 14:25:46 +0100

    Added events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,81 @@
 import "./App.css";
 
+// Define our bookOne and bookTwo objects with
+// respective values.
+
+const bookArray = [
+  {
+    img: "https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+    id: 1,
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41X65pro2mS._SY291_BO1,204,203,200_QL40_ML2_.jpg",
+    title: "The Wasp Factory",
+    author: "Iain Banks",
+    id: 2,
+  },
+];
+
 function App() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <EventExample />
+      {bookArray.map((book) => {
+        const { id, img, title, author } = book;
+        return <Book key={id} img={img} title={title} author={author} />;
+      })}
     </section>
   );
 }
 
-function Book() {
+const EventExample = () => {
+  // Define our event handlers here
+  const handleFormInput = (event) => {
+    console.log(event.target.value, event.target.name);
+  };
+
+  const handleClick = (event) => {
+    console.log("Button Clicked!", event.target);
+  };
+
+  const handleSubmit = (e) => {
+    // Prevent the default behaviour of form submission
+    e.preventDefault();
+    console.log("Form submitted", e);
+  };
+
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <section>
+      <form onSubmit={handleSubmit}>
+        <h2>Some Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={(e) => {console.log(e.target.value)}}
+          style={{ margin: "1rem 0" }}
+        />
+        <button type="submit">Submit Form</button>
+      </form>
+      <button onClick={handleClick}>Click Me!</button>
+    </section>
+  );
+};
+
+// Destructuring the props object in parentheses
+function Book({ img, title, author, children }) {
+  return (
+    <article className="book">
+      {/** Use the destructured props here */}
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+
+      {/* Place your children prop here */}
+      {children}
     </article>
   );
 }
-
-const Image = () => (
-  <img src="https://books.google.co.uk/books/content?id=jXc38U9NqisC&pg=PP1&img=1&zoom=3&hl=en&bul=1&sig=ACfU3U2FOTAnIVkT9NhHIiCm_2W3M-KizQ&w=1280" />
-);
-const Title = () => <h2>Book Title</h2>;
-const Author = () => <h4>Book Author</h4>;
 
 export default App;
